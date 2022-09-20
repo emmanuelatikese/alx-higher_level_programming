a339fd213cfc2855d0bcf70041e7fe082eec7ef3
root <emmanuelatikese14@gmail.com>
2022-09-20 16:40:57 -0700

    add

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for n in range(0, 10):
     for n2 in range(0, 10):
-        if n is 8 and n2 is 9:
+        if n == 8 and n2 == 9:
             print("{:d}{:d}".format(n, n2))
         elif n < n2:
             print("{:d}{:d}".format(n, n2), end=", ")
