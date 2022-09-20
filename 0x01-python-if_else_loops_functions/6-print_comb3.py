6df50981453f25b9838da7f7e5e4a138526b6031
root <emmanuelatikese14@gmail.com>
2022-09-20 14:31:13 -0700

    add

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ for a in range(0, 100):
         a = "0" + str(a)
     if int(a) // 10 > int(a) % 10 or int(a) // 10 == int(a) % 10:
         continue
-    if int(a) < 99:
+    if int(a) < 89:
         a = str(a) + ',' + " "
     print(('{:}').format(str(a)), end='')
 print('\n')
