52c586c3c666c5c6299bb701f6b87ad2bf4882d3
root <emmanuelatikese14@gmail.com>
2022-09-20 16:26:15 -0700

    add

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(0, 101):
+        if n % 3 == 0 and n % 5 != 0 :
+            ans = 'Fizz'
+        elif n % 5 == 0 and n % 3 != 0:
+            ans = 'Buzz'
+        elif n % 5 == 0 and n % 3 == 0:
+            ans = 'FizzBuzz'
+        else:
+            ans = str(n)
+        if n < 100:
+            ans = ans + " "
+        print(ans, end='')
