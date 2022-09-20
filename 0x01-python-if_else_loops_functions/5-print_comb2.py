125e1e545ff49b8fc36291f3c7919e2a2e93f483
root <emmanuelatikese14@gmail.com>
2022-09-20 14:07:23 -0700

    add

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for a in range(0,100):
-    if a < 9:a = "0" + str(a)
+    if a < 9: a = "0" + str(a)
     if int(a) < 99: a = str(a) + ','
     print(('{:} ').format(str(a)), end='')
+    print('\n')
