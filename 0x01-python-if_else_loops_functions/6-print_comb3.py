0b37db83627f877597f2e1414dead9ef73f61c6c
root <emmanuelatikese14@gmail.com>
2022-09-20 14:28:48 -0700

    add

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for a in range(0, 100):
     if a < 9:
         a = "0" + str(a)
-    if  int(a)//10 > int(a)%10:
+    if int(a) // 10 > int(a) % 10 or int(a) // 10 == int(a) % 10:
         continue
     if int(a) < 99:
         a = str(a) + ',' + " "
