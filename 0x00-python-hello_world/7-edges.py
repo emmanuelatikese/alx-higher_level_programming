903c4fe64187998ba31ff4d4c7f4b60820dbc5c4
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-04 08:37:14 +0000

    adding

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word: str = "Holberton"
+word_first: str = word[:3]
+word_last_2: str = word[6:]
+middle_word: str = word[1:-1]
+print(f"First letters: {word_first}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
