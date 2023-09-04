c5fe2cbb7e9def43c9352485ffb51417681f0546
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-04 08:14:48 +0000

    adding

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+'''
+output of this file:Float: 3.14
+'''
 number: int = 98
 print(f"{number} Battery street\n")
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str: str = "Holberton School"
+print(str[:9], (str[10:16]+str[:9] + " ")*3)
+
+
