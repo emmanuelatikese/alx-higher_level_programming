60fa344f8d4ea6d074769c7eef149832afe00be4
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-05 20:01:12 +0000

    adding

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -18,7 +18,7 @@ listint_t *insert_node(listint_t **head, int number){
     
     if (ptr == NULL){
         head = tmp;
-        return head;
+        return tmp;
     }
     else if (head->next == NULL &&  tmp->n <= head->n){
         tmp->next = head;
