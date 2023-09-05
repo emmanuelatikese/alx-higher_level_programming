c1631f9b5117b9e9228d0dd72d6fc0a5e907baca
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-05 14:08:17 +0000

    adding

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-for x in range(0,100):
+for x in range(0, 100):
     if x in range(1, 10):
-        print(f"0{x}, ",end="")
+        print("0{}, ".format(x), end="")
     elif x in range(10, 99):
-        print(f"{x}, ",end="")
+        print("{}, ".format(x), end="")
     else:
         print(x)
