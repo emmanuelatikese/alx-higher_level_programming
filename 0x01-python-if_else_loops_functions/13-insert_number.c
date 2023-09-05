61f584d8f6ba2a7701204ee4b91243c678ea9e61
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-05 21:49:17 +0000

    adding

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -8,40 +8,33 @@
  */
 
 listint_t *insert_node(listint_t **head, int number){
-    listint_t **ptr = head;
-    listint_t **tmp = malloc(sizeof(listint_t));
-    listint_t **prev, **next;
-    listint_t **current = ptr;
+    listint_t *ptr = *head;
+    listint_t *prev, *next;
+    listint_t *current = ptr;
+    listint_t tmp;
+    tmp->n = number;
+    tmp->next = NULL;
 
-    (**tmp).n = number;
-    (**tmp).next = NULL;
-    
-    if (ptr == NULL){
-        head = tmp;
-        return *tmp;
+    if (ptr == NULL || (ptr->next == NULL && tmp->n < ptr->n)){
+        tmp->next = ptr;
+        ptr = tmp
+        return ptr;
     }
-    if ((**head).next == NULL &&  (**tmp).n <= (**head).n){
-        (**tmp).next = head;
-        *head = *tmp;
-        return *head;
-    }
-
     while(ptr != NULL){
-        
-        *prev = *ptr;
-        *next = (**ptr).next;
-        if((**tmp).n <= (**ptr).n){
-            (**tmp).next = *prev;
-            (**current).next = tmp;
+        ptr = ptr->next;
+        prev = ptr
+        next = ptr->next;
+
+        if (tmp->n <= ptr->n){
+            current->next = tmp;
+            tmp->next = prev;
             return *head;
         }
-        *current = *prev;
-        
-        if ((**tmp).n > (**ptr).n){
-            (**tmp).next = *next;
-            (**prev).next = *tmp;
+        current = prev;
+        if(tmp->n > ptr->n){
+            prev->next = tmp;
+            tmp->next = next;
             return *head;
         }
-        *ptr = (**ptr).next;
     }
 }
