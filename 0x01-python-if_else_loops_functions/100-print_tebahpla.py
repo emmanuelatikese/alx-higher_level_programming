fdeb0ede04b5756fb4ad15ac8753ab8a63537d18
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-05 16:29:23 +0000

    adding

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for x in range(122, 96, -1):
     if (x + 1) % 2 == 0:
-        print(f"{chr(x).upper()}", end="")
+        print("{}".format(chr(x).upper()), end="")
     else:
-        print(f"{chr(x)}", end="")
+        print("{}".format(chr(x)), end="")
