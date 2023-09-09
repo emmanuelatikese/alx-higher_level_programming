9ce9fd613c6b4055f492823ae142096ce4f304d1
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-09 02:40:46 -0700

    adding

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
-        return
+        return my_list
     new_list = [y for y in my_list]
     i = 0
     while i < len(new_list):
