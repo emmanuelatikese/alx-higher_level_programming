cf1d18d294408a5f7e928b5022e150072ad3be1f
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-09 08:03:40 +0000

    add

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-	if my_list is None:
-		return
-	else:
-		for x in my_list:
-			if (x, isinstance(x, int)):
-				print("{}".format(x))
-	return
+    if my_list is None:
+        return
+    else:
+        for x in my_list:
+            if (x, isinstance(x, int)):
+                print("{}".format(x))
+    return
