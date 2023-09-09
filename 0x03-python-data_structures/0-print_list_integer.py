dea86ff3b82c945edbd7a82f0b7bbfb3fa78c036
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-09 01:51:27 -0700

    adding

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    if my_list == []:
-        return
-    else:
-        for x in my_list:
-            print("{}".format(x))
+    for x in my_list:
+        print("{}".format(x))
     return
