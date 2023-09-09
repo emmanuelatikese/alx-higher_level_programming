83d9b6e8e4ed8cc40e142cb72f88e6341530da42
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-09 09:13:54 -0700

    adding

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        return (None, len(sentence))
-    else:
-        return (sentence[0], len(sentence))
+    return (len(sentence), None if sentence == "" else sentence[0])
