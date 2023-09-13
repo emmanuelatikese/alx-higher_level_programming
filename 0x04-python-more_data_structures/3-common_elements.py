c37362698f139aad44c4bfe4b92dbd68baf779ea
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-13 10:53:59 -0700

    adding

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    result = []
+    for x in set_1:
+        for y in set_2:
+            if x == y:
+                result.append(x)
+            else:
+                continue
+    return result
