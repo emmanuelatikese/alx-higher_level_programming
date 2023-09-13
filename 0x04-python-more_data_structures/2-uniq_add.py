463cb48f2bdbf052dc74c9e1774f8ae7e067e816
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-13 10:44:28 -0700

    adding

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,7 @@ def uniq_add(my_list=[]):
     for x in my_list:
         if my_list.count(x) > 1:
             my_list.remove(x)
-    result = sum(my_list)
+    result = 0
+    for x in my_list:
+        result += x
     return result
