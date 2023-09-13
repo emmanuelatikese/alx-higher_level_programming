961b04f6b10ab1c8ed1d06397509a7e639f39b3b
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-13 12:21:29 -0700

    adding

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    new_dict = a_dictionary.copy()
-    new_dict.pop(str(key))
-    return new_dict
+    if key in a_dictionary.keys():
+        new_dict = a_dictionary.copy()
+        new_dict.pop(str(key))
+        return new_dict
+    return a_dictionary
