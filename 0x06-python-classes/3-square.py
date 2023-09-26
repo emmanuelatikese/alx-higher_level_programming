89b66825f7c6fe80eea3665fe4f1206e3443ed6a
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-26 15:29:05 -0700

    adding

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+""" same old class but new function area was added """
+
+
 class Square:
+    """check on the class"""
     def __init__(self, size):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self._Square__size = size
+
+    """this just square the size of the function that all"""
 
     def area(self): return (self._Square__size)**2
