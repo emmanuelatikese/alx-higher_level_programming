47505c2f6c68fd853675e092df5e2194500e2a16
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-26 15:37:35 -0700

    adding

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """check on the class"""
     def __init__(self, size):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
