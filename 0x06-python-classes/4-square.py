ff63f1c9e57287f391f50dd073094a4cce5510a3
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-26 15:49:07 -0700

    adding

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""same old function but new features"""
+
+
 class Square:
+    """this is where the function get started"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -7,11 +11,14 @@ class Square:
             raise ValueError("size must be >= 0")
         self._Square__size = size
 
+    """same old area function for sure"""
     def area(self): return (self._Square__size)**2
 
+    """this is a size function it get a getter funcdtion"""
     @property
     def size(self): return self._Square__size
 
+    """this is a setter for sure with all decorator in there"""
     @size.setter
     def size(self, value):
         if type(value) != int:
