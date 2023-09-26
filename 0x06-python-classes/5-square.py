6a791b1dfe17f0ccd6b87aa3a1e3c4d66e6cc39b
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-09-26 15:53:07 -0700

    adding

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
+"""same old function but new things happening"""
+
+
 class Square:
-    def __init__(self, size):
+    """initializing for sure"""
+    def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
         self._Square__size = size
 
+    """same old function you know it already"""
     def area(self): return (self._Square__size)**2
 
+    """same old nothing new"""
     @property
     def size(self): return self._Square__size
 
+    """do you need comment so badly"""
     @size.setter
     def size(self, value):
         if type(value) != int:
@@ -20,6 +27,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self._Square__size = value
 
+    """something new but not that new you know print don't you"""
     def my_print(self):
         if self._Square__size == 0:
             print()
