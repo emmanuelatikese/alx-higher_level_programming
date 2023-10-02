1c414db45c733a0cad43f8b7297f98ff077a9edf
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-02 06:53:56 -0700

    adding

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -4,6 +4,8 @@
 
 
 class Rectangle:
+    """this is comment"""
+
     def __init__(self, width=0, height=0):
         if type(height) != int:
             raise TypeError("height must be an integer")
