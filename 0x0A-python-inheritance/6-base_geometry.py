e62cc047a179826f775da908f52b05451ff6e253
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-09 10:39:14 -0700

    adding

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This is just an empty function"""
+
+
+class BaseGeometry:
+    """this function not initialize"""
+    def area(self):
+        return raise Exception("area is not implemnted")
