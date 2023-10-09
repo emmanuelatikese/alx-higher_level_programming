50aaf9179c23e555c4a958d51c93487a9d13eab6
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-09 10:08:11 -0700

    adding

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This return whether an element
+is an instance of a class or not."""
+
+
+def is_kind_of_class(obj, a_class):
+    """this will return true or false"""
+    return True if isinstance(obj, a_class) else False
