904ba80e74f4bd042fa4ae67ace15387f60d201b
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-10 13:59:15 -0700

    d

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this is another funciton """
 from sys import argv
+import os
 
 ld = __import__("6-load_from_json_file").load_from_json_file
 sav = __import__("5-save_to_json_file").save_to_json_file
@@ -8,5 +9,11 @@ name_file = "add_item.json"
 
 
 if __name__ == "__main__":
-    sav(argv[1:], name_file)
+    if os.path.exists(name_file):
+        d = ld(name_file)
+        for x in argv[1:]:
+            d.append(x)
+        sav(d, name_file)
+    else:
+        sav(argv[1:], name_file)
     ld(name_file)
