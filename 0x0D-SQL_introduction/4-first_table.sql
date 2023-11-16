908a137521998279858ca1fb98cf05aa4d36858d
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-11-16 14:47:31 -0800

    adding

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- This file creates a table 
 -- this is the code below
-CREATE table hbtn_0c_0.first_table (id INT, name VARCHAR(256));
+CREATE table if not exists hbtn_0c_0.first_table (id INT, name VARCHAR(256));
