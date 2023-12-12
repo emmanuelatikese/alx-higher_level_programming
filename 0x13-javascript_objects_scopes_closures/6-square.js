38ac194e4883f8be0bda13f345e2135945546bce
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-12 14:58:40 -0800

    adding

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,13 +5,12 @@ const Sq = require('./5-square');
 class Square extends Sq {
   constructor (size) {
     super(size);
-    this.size_ = size;
   }
 
   charPrint (c) {
     const ch = c === undefined ? 'X' : c;
-    for (let i = 0; i < this.size_; i++) {
-      const str = ch.repeat(this.size_).replace(' ', '');
+    for (let i = 0; i < this.width; i++) {
+      const str = ch.repeat(this.width).replace(' ', '');
       console.log(str);
     }
   }
