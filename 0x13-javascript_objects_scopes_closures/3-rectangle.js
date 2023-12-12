244eff768f912c86395d9295882be63605929f33
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-12 10:17:37 -0800

    sss

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -5,12 +5,12 @@ class Rectangle {
     this.width = w > 0 ? w : undefined;
     this.height = h > 0 ? h : undefined;
   }
+
   print () {
-   for (let i = 0; i < this.height; i++) {
-	const str = 'X'.repeat(this.width).replace(" ","");
-	console.log(str);
-   } 
+    for (let i = 0; i < this.height; i++) {
+      const str = 'X'.repeat(this.width).replace(' ', '');
+      console.log(str);
+    }
   }
 }
-
 module.exports = Rectangle;
