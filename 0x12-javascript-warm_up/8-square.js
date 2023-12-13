e3b6e507ece2bb29cd9ed492675fc9ef3b3cb107
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-13 14:33:38 -0800

    adding

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 
 if (parseInt(process.argv[2])) {
   for ( let i = 0; i < parseInt(process.argv[2]); i++) {
-    console.log('X'.repeat(parseInt(process.argv[2])).replace(' ',''));
+    console.log('X'.repeat(parseInt(process.argv[2])).replace(' ', ''));
   }
 } else {
   console.log('Missing size');
