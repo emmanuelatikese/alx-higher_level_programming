d240032f326f9dc61e36c07c418a951a66e233f8
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-13 15:14:22 -0800

    adding

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,14 +1,3 @@
 #!/usr/bin/node
 
-// searching for the second biggest
-
-const { argv } = require('node:process');
-
-if (argv.length <= 3) {
-  console.log(0);
-} else {
-
-  let list = argv.slice(2, argv.length).sort((a, b) => b - a);
-
-  console.log(list[1]);
-}
+process.argv.length <= 3 ? console.log(0) : console.log(process.argv.slice(2, process.argv.length).sort((a, b) => b - a)[1]);
