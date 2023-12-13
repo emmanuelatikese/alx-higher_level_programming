fcd04c1769d215444870ab484cf852aa7a9cfc2c
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-13 13:56:22 -0800

    adding

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,14 +1,3 @@
 #!/usr/bin/node
-// working on argv
 
-const { argv } = require('node:process');
-let count = 0;
-
-argv.map(x => count++);
-
-if (count === 2) {
-  console.log('No argument');
-} else {
-  const list = argv.slice(2, count);
-  list.forEach(x => console.log(x));
-}
+process.argv[2] ? console.log(process.argv[2]) : console.log('No argument');
