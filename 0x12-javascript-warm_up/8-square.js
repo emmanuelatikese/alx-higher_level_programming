404067552405d7756c69b067c265ec53118cbf59
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-13 14:34:07 -0800

    adding

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (parseInt(process.argv[2])) {
-  for ( let i = 0; i < parseInt(process.argv[2]); i++) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
     console.log('X'.repeat(parseInt(process.argv[2])).replace(' ', ''));
   }
 } else {
