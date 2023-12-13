ff9b4177cb4e0fc7b298850adaaa72b7b08acce8
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-13 14:15:40 -0800

    adding

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
-// number of times to print
 
-const { argv } = require('node:process');
-
-if (parseInt(argv[2])) {
-  let i = 0;
-  const num = parseInt(argv[2]);
-  while (i < num) {
-    console.log('C is fun');
-    i++;
+if (parseInt(process.argv[2])) {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+	console.log('C is fun');
   }
-} else {
+  } else {
   console.log('Missing number of occurrences');
 }
