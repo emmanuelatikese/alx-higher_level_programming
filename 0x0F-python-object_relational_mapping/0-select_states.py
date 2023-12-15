08c7a6a10c51b8f3a5ab55931faec77b61566feb
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-15 06:55:26 -0800

    adding files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import MySQLdb
-from sys import argv
+import sys
 ''' This is all about manipulation of database in python'''
 
 if __name__ == '__main__':
-    cn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+    cn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     curs = cn.cursor()
     curs.execute("SELECT * FROM states ORDER BY id ASC")
     _rows = curs.fetchall()
