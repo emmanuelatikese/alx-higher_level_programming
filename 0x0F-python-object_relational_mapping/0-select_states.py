b6d9a901fbf7c5ae7c1521dff60c3afd3a8278ab
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-15 13:23:56 -0800

    adding

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 ''' This is all about manipulation of database in python'''
 
 if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
     cn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     curs = cn.cursor()
