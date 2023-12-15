582c05fe94efc19a06d08851866c24a892f747b9
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-15 14:06:08 -0800

    adding

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,10 +10,8 @@ if __name__ == '__main__':
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     curs = cn.cursor()
     curs.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
     _row = curs.fetchall()
-
-    for x in _row:
-        print(x)
+    for new_row in _row:
+        print(new_row)
     curs.close()
     cn.close()
