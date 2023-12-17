a6efab4a4293951649a7dcbdba972ad91f6a7747
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-17 14:41:57 -0800

    adding stuffs

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''This is another alchemy'''
+
+if __name__ == '__main__':
+    from sys import argv
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from model_state import Base, State
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+                            argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    st = session.query(State).filter_by(id=1).order_by(State.id).all()[0]
+    print("{}: {}".format(st.id, st.name)) if st else print('nothing')
+    session.close()
