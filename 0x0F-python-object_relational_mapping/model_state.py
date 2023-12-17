0d3e7113d4f1a82379302863db1e66f8820ec202
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-12-17 13:40:04 -0800

    adding

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+''' This is the begining of orm'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    '''This is a orm state class'''
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
