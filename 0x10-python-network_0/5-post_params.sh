801d1fcb2b29c86782acd49367531514ab5793aa
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-05 15:27:47 -0800

    adding

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #commenting a lot of things
-curl -X "POST" "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
+curl -sX "POST" "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
