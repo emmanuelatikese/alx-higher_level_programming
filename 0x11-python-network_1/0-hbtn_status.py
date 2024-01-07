f631522798461717f26f91ba41a0a1dbb4d36859
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-07 02:27:44 -0800

    is all about request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,10 @@ import urllib.request
 if __name__ == '__main__':
     url = "https://alx-intranet.hbtn.io/status"
     req = urllib.request.Request(url)
-    result = """Body response:
-    - type: {}
-    - content: {}
-    - utf8 content: {}"""
 
     with urllib.request.urlopen(req) as f:
         page = f.read()
-    print(result.format(type(page), page, page.decode()))
+    print("""Body response:
+    - type: {}
+    - content: {}
+    - utf8 content: {}""".format(type(page), page, page.decode()))
