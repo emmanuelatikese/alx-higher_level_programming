5c0cab0d83e2b3687d8e4f06c8c50c36ae16bca2
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-07 03:08:56 -0800

    is all about request

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''We posting things out here'''
-from urllib.request import Request, urlopen
-from urllib.parse import urlencode
-from sys import argv
 
 if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    from sys import argv
+
     url = argv[1]
     data = urlencode({'email': argv[2]}).encode('ascii')
     req = Request(url, data)
