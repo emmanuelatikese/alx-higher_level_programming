35ee731256ad842e77360755ac35d44dbfaf483d
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-07 13:57:02 -0800

    adding

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 '''We working on the request lib'''
+import requests
 
 if __name__ == '__main__':
-    import requests
     req = requests.get("https://alx-intranet.hbtn.io/status")
     print('Body response:')
-    print('    - type: {}'.format( type(req.text)))
+    print('    - type: {}'.format(type(req.text)))
     print('    - content: {}'.format(req.text))
