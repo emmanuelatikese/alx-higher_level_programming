91b7d9023c558e50246826bdb77e3ce11fd86d60
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-07 02:22:25 -0800

    is all about request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,14 +2,14 @@
 """This is all about urllib norhing else"""
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-
-req = urllib.request.Request(url)
-result = """Body response:
+if __name__ == '__main__':
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    result = """Body response:
     - type: {}
     - content: {}
     - utf8 content: {}"""
 
-with urllib.request.urlopen(req) as f:
-    page = f.read()
-print(result.format(type(page), page, page.decode()))
+    with urllib.request.urlopen(req) as f:
+        page = f.read()
+    print(result.format(type(page), page, page.decode()))
