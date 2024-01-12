c9cd1c6a45ff7b9256c11ce9123880b253d502c0
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-12 03:44:16 -0800

    adding files

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 '''using of api auth'''
 if __name__ == '__main__':
-
     import requests
     from sys import argv
     username = argv[1]
     password = argv[2]
-    req = requests.get('https://api.github.com/users', auth=(username, password))
+    req = requests.get('https://api.github.com/user', auth=(username, password))
     res = req.json()
-    print(res[0].get('id'))
+    print(res.get('id'))
