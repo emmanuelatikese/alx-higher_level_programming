51bac001b417e3e0634eba08ed7492ae52b2d46f
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-12 15:51:24 -0800

    adding

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''This is all about the commits'''
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    v1, v2 = argv[1], argv[2]
+    req = requests.get('https://api.github.com/repos/{}/{}/commits'
+                       .format(v1, v2))
+    res = req.json()
+    count = 0
+    for x in res:
+        if count < 10:
+            author = x['commit']['author']['name']
+            print('{}: {}'.format(x['sha'], author))
+            count += 1
+        else:
+            break
