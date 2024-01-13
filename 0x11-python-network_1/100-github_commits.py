44a143cd02809e35a8b33a112ba75b3604fb28ba
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-12 16:10:50 -0800

    adding

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
             sha = res[x].get('sha')
             author = res[x].get('commit').get('author').get('name')
             print('{}: {}'.format(sha, author))
-    except IndexError:
+    except:
         pass
