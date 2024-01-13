b62a42e2deea352a1411aebdb58ec1c0ae4dbebf
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-12 16:08:29 -0800

    adding

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,13 +4,13 @@
 if __name__ == '__main__':
     import requests
     from sys import argv
-    v1 = argv[1]
-    v2 = argv[2]
-    req = requests.get('https://api.github.com/repos/{}/{}/commits'
-                       .format(v1, v2))
-    res = req.json()
-    count = 0
+
     try:
+        v1 = argv[1]
+        v2 = argv[2]
+        req = requests.get('https://api.github.com/repos/{}/{}/commits'
+                           .format(v1, v2))
+        res = req.json()
         for x in range(0, 10):
             sha = res[x].get('sha')
             author = res[x].get('commit').get('author').get('name')
