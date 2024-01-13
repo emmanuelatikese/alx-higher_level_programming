14e5c4a9a011719485b65093a83932332d75041e
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-12 16:06:24 -0800

    adding

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,10 +10,10 @@ if __name__ == '__main__':
                        .format(v1, v2))
     res = req.json()
     count = 0
-    for x in res:
-        if count < 10:
-            author = x['commit']['author']['name']
-            print('{}: {}'.format(x['sha'], author))
-            count += 1
-        else:
-            break
+    try:
+        for x in range(0, 10):
+            sha = res[x].get('sha')
+            author = res[x].get('commit').get('author').get('name')
+            print('{}: {}'.format(sha, author))
+    except IndexError:
+        pass
