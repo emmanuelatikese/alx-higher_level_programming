0ef67c6a091c42bab4b2f3895166b5f03f2ec97c
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-01-30 16:05:06 -0800

    adding

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,11 +2,11 @@
 
 const args = process.argv.slice(2);
 const req = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/films/?id=' + args[0];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + args[0];
 
 req(url, (err, res, body) => {
   if (err) {
     console.log(err);
   }
-  console.log(JSON.parse(body).results[0].title);
+  console.log(JSON.parse(body).title);
 });
