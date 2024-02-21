01d741ebe0d33a4333d4acb45feaa65e05bc3a93
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-02-21 15:23:05 -0800

    adding

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-import sys
-s = "and that piece of art is useful - Dora Korpar, 2015-10-19"
-lt = s.split(" ")
-for i in range(len(lt)):
-    if i < len(l) and i != 0:
-        sys.stderr.write(" ")
-    sys.stderr.write(lt[i])
-sys.stderr.write('\n')
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
