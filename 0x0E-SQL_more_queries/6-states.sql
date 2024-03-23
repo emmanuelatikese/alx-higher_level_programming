8547644df60ca32d5deec1897cf29965f2481ec4
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-23 12:11:46 -0700

    adding

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creating a table
 CREATE TABLE IF NOT EXISTS states (
         id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-        name VARCHAR(256),
+        name VARCHAR(256) not null,
         UNIQUE(id)
 );
