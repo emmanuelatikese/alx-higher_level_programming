a621e8464935a342d048fdb3899f4eb74cf3245f
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-23 10:14:23 -0700

    adding

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creating a user
+-- with a pwd
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
