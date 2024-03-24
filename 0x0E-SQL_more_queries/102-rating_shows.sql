08116ce3f7ff494309091e21440ec74e1ce9ac47
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 14:55:07 -0700

    adding files

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,5 @@
 -- suming all rating
-SELECT title, SUM(tv_show_ratings.rate) AS rating 
+SELECT title, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_shows LEFT JOIN tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
 ORDER BY rating
