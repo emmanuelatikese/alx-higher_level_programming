8d45f0dc63e1e83dccdc0cca581d34fbb44054a8
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 10:31:35 -0700

    adding

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,6 +3,6 @@ SELECT title
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.name = 'Comedy'
+WHERE tv_genres.name = 'Comedy'
 GROUP BY title
 ORDER BY title ASC;
