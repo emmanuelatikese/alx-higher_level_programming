afb27ea229a8fe9448b7e0163c8bee2e059fe9d0
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 10:21:13 -0700

    adding

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows ON tv_show_genres.title = tv_shows.title
 WHERE tv_shows.title = 'Dexter'
+GROUP BY name
 ORDER BY name ASC;
