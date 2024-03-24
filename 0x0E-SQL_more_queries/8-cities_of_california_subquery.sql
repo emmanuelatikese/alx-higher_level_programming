a9a8873885356487931430109ce8bbc82eebbe9b
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 07:44:49 -0700

    adding

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- listing cities from california
-select id, name from hbtn_0d_usa.cities
-where state_id = 1 order by id asc;
+SELECT id, name FROM hbtn_0d_usa.cities
+WHERE state_id = 1 ORDER BY id ASC;
