26cb07e1e6d69fed7280ab824a98598b1838c6e0
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 07:22:25 -0700

    adding

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,9 @@
 -- creating a database and a table known as cities
-create DATABASE IF NOT EXISTS hbtn_0d_usa;
-create TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	name VARCHAR(256) NOT NULL,
 	state_id INT NOT NULL,
