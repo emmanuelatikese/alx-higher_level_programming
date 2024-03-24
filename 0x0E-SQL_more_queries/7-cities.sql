d104c246003c0896953e35cac654b938b39a8c9c
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 07:13:36 -0700

    adding

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,7 @@
 create database if not exists hbtn_0d_usa;
 create table if not exists hbtn_0d_usa.cities (
 	id int auto_increment primary key not null,
+	unique(id),
 	name varchar(256) not null,
 	state_id int,
 	foreign key (state_id) references states(id)
