d9171187bcdc0fb3163261a722365040c5f36f4f
emmanuelatikese <emmanuelatikese14@gmail.com>
2024-03-24 10:02:10 -0700

    adding

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- still working on listing
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id)
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS 'num'
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY COUNT(tv_show_genres.genre_id) DESC;
+ORDER BY num DESC;
